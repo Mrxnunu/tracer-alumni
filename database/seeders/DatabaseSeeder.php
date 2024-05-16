aa19b85b3c118e1a7a7c9218c061c149e9934bc5
Ibnu Mahjub <ibnumahjub23@gmail.com>
2024-05-16 15:02:29 +0700

    seeder artikel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,59 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'mrxnunu',
+            'username' => 'mrxnununs',
+            'email' => 'mrxnunu@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'bagas',
+            'username' => 'gassd',
+            'email' => 'bagas@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Bintang Sport',
+            'slug' => 'bintang-sport'
+        ]);
+        Category::create([
+            'name' => 'Kesehatan Jasmani',
+            'slug' => 'kesehatan-jasmani'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque ducimus recusandae ex excepturi, asperiores reiciendis officiis atque perspiciatis tempora temporibus totam possimus consectetur beatae saepe corrupti. Nemo, aspernatur fugiat.',
+            'category_id' => 1,
+            'user_id' => 1,
+        ]);
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque ducimus recusandae ex excepturi, asperiores reiciendis officiis atque perspiciatis tempora temporibus totam possimus consectetur beatae saepe corrupti. Nemo, aspernatur fugiat.',
+            'category_id' => 2,
+            'user_id' => 1,
+        ]);
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque',
+            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum quia hic odit consequuntur id voluptatum quaerat fugiat laborum enim doloremque ducimus recusandae ex excepturi, asperiores reiciendis officiis atque perspiciatis tempora temporibus totam possimus consectetur beatae saepe corrupti. Nemo, aspernatur fugiat.',
+            'category_id' => 3,
+            'user_id' => 2,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Post;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/artikel', function () {
+//     return view('posts');
+// });
+
+Route::get('/artikel', [PostController::class, 'index']);
