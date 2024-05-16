8d4a9ce9164edf25dd4976d35c361d528be1b3a5
Ibnu Mahjub <ibnumahjub23@gmail.com>
2024-05-16 15:10:22 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-// Route::get('/artikel', function () {
-//     return view('posts');
-// });
-
-Route::get('/artikel', [PostController::class, 'index']);
+// 
