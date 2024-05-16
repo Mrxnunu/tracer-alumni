1f59bbd9bae5d0bf2213c06c31e48b0ed2d98e39
Ibnu Mahjub <ibnumahjub23@gmail.com>
2024-05-16 15:14:33 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\Post;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\TracerAlumniController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
+Route::get('/', [TracerAlumniController::class, 'show']);
