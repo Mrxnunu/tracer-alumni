ed1a545d743358a01d4dafd7ca1e2e237a2e2f56
bagasm24 <mbagas368@gamil.com>
2024-05-16 15:02:08 +0700

    buat navbar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TracerAlumniController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
+Route::get('/', [TracerAlumniController::class, 'show']);
