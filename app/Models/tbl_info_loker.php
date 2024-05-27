a676311140faad0c27b0c00135517b614d38153b
Ibnu Mahjub <ibnumahjub23@gmail.com>
2024-05-27 14:48:18 +0700

    senin menambahkan slug otomatis

--- app/Models/tbl_info_loker.php
+++ app/Models/tbl_info_loker.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class tbl_info_loker extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
     protected $guarded = ['id'];
 
     public function author()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
