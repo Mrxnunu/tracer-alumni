c14cc68480e9978243261b1b306835a04710cf1a
bagasm24 <mbagas368@gamil.com>
2024-05-31 09:09:43 +0700

    update

--- resources/views/dashboard/alumni/index.blade.php
+++ resources/views/dashboard/alumni/index.blade.php
@@ -80,7 +80,7 @@
                                 <a href="{{ url('/dashboard/alumni/'.$a->npm) }}" class="flex-shrink-0 px-4 py-2 font-medium text-white bg-green-500 rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-green-200">
                                     Detail
                                 </a>
-                            </td>   
+                            </td>  
                         </tr>
                         @endforeach
                     </tbody>
